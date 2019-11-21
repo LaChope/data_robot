62063b2879a605e5bee5c846a8cc01944d2e0773
LaChope <57750426+LaChope@users.noreply.github.com>
2019-11-21 17:04:30 +0100

    Slide 6

--- data_graph/data_graph.py
+++ data_graph/data_graph.py
@@ -1,35 +1,25 @@
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import numpy as np
 
+def plotSlide6():
 
-class PlotData():
-    
-    def plotWeeklyResults(self):
+    #Get the .csv files
+    df_overall = pd.read_csv('C:\\Alten\\Internal_Project\\Data_repository\\Results_DB\\CSV\\WeeklyReportResults.csv')
+    df_DAI = pd.read_csv('C:\\Alten\\Internal_Project\\Data_repository\\Results_DB\\CSV\\ResultsIDC5.csv')
+    df_MAP = pd.read_csv('C:\\Alten\\Internal_Project\\Data_repository\\Results_DB\\CSV\\ResultsMAP.csv')
+    df_JLR = pd.read_csv('C:\\Alten\\Internal_Project\\Data_repository\\Results_DB\\CSV\\ResultsJLR.csv')
 
-        df = pd.read_csv('C:\\Alten\\Internal_Project\\Data_repository\\WeeklyReportResults.csv')
+    #Initialize figure with subplots
+    fig = make_subplots(rows=2, cols=3, subplot_titles=("Total close", "Number of Opened Tickets / Week", "Total in Progress", "Opened HAFMap-tickets and SP" "Opened DAI-tickets and SP""Opened JLR-tickets and SP"))
 
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x = df["CW"], y = df["OpenTicketsTotal"], name = "OpenTicketsTotal", mode = "lines"))
-        fig.add_trace(go.Scatter(x = df["CW"], y = df["InProgressTicketsTotal"], name = "InProgressTicketsTotal", mode = "lines"))
-        fig.add_trace(go.Scatter(x = df["CW"], y = df["ClosedTicketsTotal_perCW"], name = "ClosedTicketsTotal_perCW", mode = "lines"))
-        fig.add_trace(go.Scatter(x = df["CW"], y = df["ClosedTicketsTotal_perCW"], name = "ClosedTicketsTotal_perCW", mode = "lines"))
+    #Add traces for Total Close
+    fig.add_trace(go.Scatter(x = df_overall["CW"], y = df_DAI["ClosedTicketsTotal_perCW"], name = "DAI", mode = "lines"), row=1, col=1)
+    fig.add_trace(go.Scatter(x = df_overall["CW"], y = df_JLR["ClosedTicketsTotal_perCW"], name = "JLR", mode = "lines"), row=1, col=1)
+    fig.add_trace(go.Scatter(x = df_overall["CW"], y = df_MAP["ClosedTicketsTotal_perCW"], name = "MAP", mode = "lines"), row=1, col=1)
 
-        fig.update_layout(title = "WeeklyReport", showlegend=True)
-        fig.show()
+    fig.update_layout(title_text="test")
+    fig.show()
 
-    def plotIssueInGivenCW(self):
-
-        df = pd.read_csv('C:\\Alten\\Internal_Project\\Data_repository\\Issue.csv')
-
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x = df["CW"], y = df["OpenTicketsTotal"], name = "OpenTicketsTotal", mode = "lines"))
-        fig.add_trace(go.Scatter(x = df["CW"], y = df["InProgressTicketsTotal"], name = "InProgressTicketsTotal", mode = "lines"))
-        fig.add_trace(go.Scatter(x = df["CW"], y = df["ClosedTicketsTotal_perCW"], name = "ClosedTicketsTotal_perCW", mode = "lines"))
-        fig.add_trace(go.Scatter(x = df["CW"], y = df["ClosedTicketsTotal_perCW"], name = "ClosedTicketsTotal_perCW", mode = "lines"))
-
-        fig.update_layout(title = "WeeklyReport", showlegend=True)
-        fig.show()
-
-plot = PlotData()
-plot.plotWeeklyResults
+plotSlide6()
