70db52fd6cbe0e56582e86ae5ed5a088c725c732
LaChope <57750426+LaChope@users.noreply.github.com>
2019-11-29 10:15:30 +0100

    DashBoard OverallPerWeek Done

--- data_graph/layouts/home.py
+++ data_graph/layouts/home.py
@@ -31,7 +31,7 @@ layout = html.Div([
             html.H1(
                 'Test Center Weekly Status 2019 CW 47',
                 style={'font-family': 'Helvetica',
-                       "margin-top": "25",
+                       "margin-top": "0",
                        "margin-bottom": "0",
                        'display': 'inline-block'
                        },
@@ -43,19 +43,22 @@ layout = html.Div([
                     'width': '15%',
                     'float': 'right',
                     'position': 'relative',
-                    'display': 'inline-block'
+                    'display': 'inline-block',
                 },
             ),
+            #Overview
             html.P(
                 'Overview',
                 style={'font-family': 'Helvetica',
-                       "font-size": "200%",
-                       "width": "100%"},
+                       "font-size": "300%",
+                       "width": "100%",
+                       "margin-top": "1%"
+                       },
             ),
         ],
     ),
         
-    #Database Subplot
+    #Tickets per Week Review sunplot
     html.Div(
         [
             html.Div(
@@ -66,7 +69,7 @@ layout = html.Div([
                 ],
             ),
             html.Div(
-                    [
+                [
                         dcc.RangeSlider(
                             id='CW-RangeSlider-OverallPerWeek',
                             min=df_overall['CW'].min(),
@@ -75,12 +78,45 @@ layout = html.Div([
                             marks={str(CW): str(CW) for CW in df_overall['CW'].unique()},
                             step=None
                         )
-                    ],
-                    style={'width': '80%', 'display': 'inline-block'}
-                ),
+                ],
+                style={'width': '80%', 'margin-left': '8%'}
+            ),
         ],
-        style={'width': '49%', 'display': 'inline-block'}
+        style={'width': '100%', 'margin-top': '5%'}
     ),
+    
+    #Test Requirements progress - total subplot
+    html.Div(
+        [
+            html.P(
+                'Test Requirements progress - total',
+                style={'font-family': 'Helvetica',
+                       "font-size": "300%",
+                       "width": "100%",
+                       "margin-top": "10%"
+                       },
+            ),
+            html.Div(
+                [
+                    dcc.Graph(
+                        id='datatable-subplots-Total'
+                    )
+                ],
+            ),
+            html.Div(
+                [
+                    dcc.RangeSlider(
+                        id='CW-RangeSlider-OverallTotal',
+                        min=df_overall['CW'].min(),
+                        max=df_overall['CW'].max(),
+                        value=[df_overall['CW'].min(), df_overall['CW'].max()],
+                        marks={str(CW): str(CW) for CW in df_overall['CW'].unique()},
+                        step=None
+                    )
+                ]
+            )
+        ]
+    )
 ]) 
 
 
@@ -93,16 +129,54 @@ layout = html.Div([
 
 def update_figure(value):
     filtered_df = df_overall[(df_overall['CW'] >= value[0]) & (df_overall['CW'] <= value[1])]
-    fig = go.Figure()
+    fig = plotly.subplots.make_subplots(
+        rows=2, cols=3,
+        specs=[[{"secondary_y": True}, {"secondary_y": True}, {"secondary_y": True}],
+                [{"secondary_y": True}, {"secondary_y": True}, {"secondary_y": True}]],
+        subplot_titles=("Total close", "Number of Opened Tickets / Week", "Total in Progress", "Opened HAFMap-tickets and SP","Opened DAI-tickets and SP","Opened JLR-tickets and SP"
+        )
+    )
 
     #Add traces for Total Close
-    fig.add_trace(go.Scatter(x = filtered_df['CW'], y = filtered_df["ActualSummaryReq_SPsum"], name = "Actual Summary", line={'color': 'orangered'}))
-    fig.add_trace(go.Bar(x = filtered_df['CW'], y = filtered_df["ActualTestedReq_SPsum"], name = "Actual tested", marker=dict(color='skyblue')))
-    fig.add_trace(go.Bar(x = filtered_df['CW'], y = filtered_df["ActualInProgress_SPsum"], name = "Actual in progress", marker=dict(color='orange')))
-    fig.add_trace(go.Bar(x = filtered_df['CW'], y = filtered_df["ActualBlockedReq_SPsum"], name = "Actual blocked", marker=dict(color='grey')))
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_DAI["ClosedTicketsTotal_perCW"], name = "DAI", line={'color': 'green'}), row=1, col=1)
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_JLR["ClosedTicketsTotal_perCW"], name = "JLR", line={'color': 'purple'}), row=1, col=1)
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_MAP["ClosedTicketsTotal_perCW"], name = "MAP", line={'color': 'grey'}), row=1, col=1)
+
+    #Add traces for Number of Opened Tickets / Week
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_DAI["OpenTickets_perCW_Count"], name = "DAI", line={'color': 'green'}, showlegend=False), row=1, col=2)
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_JLR["OpenTickets_perCW_Count"], name = "JLR", line={'color': 'purple'}, showlegend=False), row=1, col=2)
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_MAP["OpenTickets_perCW_Count"], name = "MAP", line={'color': 'grey'}, showlegend=False), row=1, col=2)
+
+    #Add traces for Total in Progress
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_DAI["InProgressTicketsTotal"], name = "DAI", line={'color': 'green'}, showlegend=False), row=1, col=3)
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_JLR["InProgressTicketsTotal"], name = "JLR", line={'color': 'purple'}, showlegend=False), row=1, col=3)
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_MAP["InProgressTicketsTotal"], name = "MAP", line={'color': 'grey'}, showlegend=False), row=1, col=3)
+
+    #Add traces for Opened HAFMap-tickets and SP
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_MAP["OpenTickets_perCW_SPsum"], name = "MAP", line={'color': 'grey'}, showlegend=False), row=2, col=1, secondary_y=False)
+    fig.add_trace(go.Bar(x = filtered_df["CW"], y = df_MAP["OpenTicketsTotal"], name = "Tickets", marker=dict(color='skyblue')), row=2, col=1, secondary_y=True)
+
+    #Add traces for Opened DAI-tickets and SP
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_DAI["OpenTickets_perCW_SPsum"], name = "DAI", showlegend=False), row=2, col=2, secondary_y=False)
+    fig.add_trace(go.Bar(x = filtered_df["CW"], y = df_DAI["OpenTicketsTotal"], name = "DAI", marker=dict(color='skyblue'), showlegend=False), row=2, col=2, secondary_y=True)
+
+    #Add traces for Opened JLR-tickets and SP
+    fig.add_trace(go.Scatter(x = filtered_df["CW"], y = df_JLR["OpenTickets_perCW_SPsum"], name = "JLR", line={'color': 'purple'}, showlegend=False), row=2, col=3, secondary_y=False)
+    fig.add_trace(go.Bar(x = filtered_df["CW"], y = df_JLR["OpenTicketsTotal"], name = "JLR", marker=dict(color='skyblue'), showlegend=False), row=2, col=3, secondary_y=True)
 
     fig.update_layout(
-        transition={'duration':1000}
-    )
-            
+        title=dict(
+            text='Number of opened DAI, JLR, MAP - tickets per week',
+            font={'family': 'Helvetica', 'size': 25},
+            x=0.5,
+            y=1
+        ),
+        transition={'duration':1000},
+        legend=dict(
+            x=-0.1, 
+            y=1,
+        ),
+    )   
+
     return fig
+
